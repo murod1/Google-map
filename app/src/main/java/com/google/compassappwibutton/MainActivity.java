02b788f9be10ce4763b31de04bb6deda3709fe46
murod1 <jmurod2000@gmail.com>
2021-04-20 11:56:08 +0900

    Google map

---
+++ app/src/main/java/com/google/compassappwibutton/MainActivity.java
@@ -0,0 +1,38 @@
+package com.google.compassappwibutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnOpenCompass, btnMap;
+    Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnOpenCompass = findViewById(R.id.btnOpenCompassView);
+        btnOpenCompass.setOnClickListener(this);
+        btnMap = findViewById(R.id.btnMap);
+        btnMap.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnOpenCompassView:
+                intent = new Intent(this, CompassActivity.class);
+                startActivity(intent);break;
+            case R.id.btnMap:
+                intent = new Intent(this, MapsActivity.class);
+                break;
+            default: break;
+        }
+        startActivity(intent);
+    }
+}
